633ea5fddd63deb248535cfca6d54c49122ae0e9
Saksham Aurora <49725699+sakshm-aurora@users.noreply.github.com>
2020-05-14 13:58:05 +0530

    Create Remove_Duplicates_Array.cpp

---
+++ Remove_Duplicates_Array.cpp
@@ -0,0 +1,48 @@
+/*
+Remove duplicate elements from sorted Array
+Given a sorted array A of size N. The function remove_duplicate takes two arguments . The first argument is the sorted array A[ ] and the second argument is 'N' the size of the array and returns the size of the new converted array A[ ] with no duplicate element.
+
+Input Format:
+The first line of input contains T denoting the no of test cases . Then T test cases follow . The first line of each test case contains an Integer N and the next line contains N space separated values of the array A[ ] .
+
+Output Format:
+For each test case output will be the transformed array with no duplicates.
+
+Your Task:
+Your task to complete the function remove_duplicate which removes the duplicate  elements from the array .
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 104
+1 <= A[ ] <= 106
+
+Example:
+Input  (To be used only for expected output) :
+2
+5
+2 2 2 2 2 
+3
+1 2 2
+Output
+2
+1 2
+*/
+
+int remove_duplicate(int arr[],int n)
+{
+    if (n==0 || n==1) 
+        return n; 
+  
+    // To store index of next unique element 
+    int j = 0; 
+  
+    // Doing same as done in Method 1 
+    // Just maintaining another updated index i.e. j 
+    for (int i=0; i < n-1; i++) 
+        if (arr[i] != arr[i+1]) 
+            arr[j++] = arr[i]; 
+  
+    arr[j++] = arr[n-1]; 
+  
+    return j; 
+} 
