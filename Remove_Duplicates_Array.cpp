b9f7ed0884b44bae736a1528b11c080a5c2ae4ad
Saksham Aurora <49725699+sakshm-aurora@users.noreply.github.com>
2020-05-14 13:58:42 +0530

    Update Remove_Duplicates_Array.cpp

--- Remove_Duplicates_Array.cpp
+++ Remove_Duplicates_Array.cpp
@@ -33,11 +33,8 @@ int remove_duplicate(int arr[],int n)
     if (n==0 || n==1) 
         return n; 
   
-    // To store index of next unique element 
     int j = 0; 
   
-    // Doing same as done in Method 1 
-    // Just maintaining another updated index i.e. j 
     for (int i=0; i < n-1; i++) 
         if (arr[i] != arr[i+1]) 
             arr[j++] = arr[i]; 
